01c02de15d75da1d9d8735bde183d268b158baa7
BrijenMakwana <brijenma@gmail.com>
2024-07-09 11:09:04 +0530

    code refactor

--- src/App.tsx
+++ src/App.tsx
@@ -1,49 +1,14 @@
-import React, {useState} from 'react';
-import {FlatList, View} from 'react-native';
+import React from 'react';
 import {QueryClient, QueryClientProvider} from '@tanstack/react-query';
 import {createStackNavigator} from '@react-navigation/stack';
 import {NavigationContainer} from '@react-navigation/native';
-
-import SearchBar from './components/SearchBar';
-import Book from './components/Book';
-import useSearch from './hooks/useSearch';
-import Divider from './components/Divider';
+import HomeScreen from './screens/HomeScreen';
 import BookScreen from './screens/BookScreen';
 import BookShelvesScreen from './screens/BookShelvesScreen';
 
 const queryClient = new QueryClient();
 
 const Stack = createStackNavigator();
-
-const Books = () => {
-  const [search, setSearch] = useState('');
-
-  const {data, refetch} = useSearch(search);
-
-  return (
-    <View
-      style={{
-        flex: 1,
-        backgroundColor: '#FFF6F6',
-      }}>
-      <SearchBar setSearch={setSearch} onPress={refetch} />
-
-      <FlatList
-        data={data}
-        numColumns={2}
-        renderItem={({item}) => <Book {...item.volumeInfo} id={item.id} />}
-        keyExtractor={item => item.id}
-        ItemSeparatorComponent={Divider}
-        columnWrapperStyle={{
-          gap: 5,
-        }}
-        contentContainerStyle={{
-          gap: 5,
-        }}
-      />
-    </View>
-  );
-};
 
 const App = () => {
   return (
@@ -53,7 +18,7 @@ const App = () => {
           screenOptions={{
             headerShown: false,
           }}>
-          <Stack.Screen name="Home" component={Books} />
+          <Stack.Screen name="Home" component={HomeScreen} />
           <Stack.Screen name="Book" component={BookScreen} />
           <Stack.Screen name="BookShelves" component={BookShelvesScreen} />
         </Stack.Navigator>
