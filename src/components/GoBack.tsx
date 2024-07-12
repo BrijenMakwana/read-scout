6aab60c35aa52434ec697a84a45df3acb545daff
BrijenMakwana <brijenma@gmail.com>
2024-07-12 10:44:59 +0530

    icons added

--- src/components/GoBack.tsx
+++ src/components/GoBack.tsx
@@ -1,13 +1,14 @@
 import {useNavigation} from '@react-navigation/native';
 import React from 'react';
-import {Pressable, StyleSheet, Text} from 'react-native';
+import {Pressable, StyleSheet} from 'react-native';
+import Icon from 'react-native-vector-icons/Ionicons';
 
 const GoBack = () => {
   const navigation = useNavigation();
 
   return (
     <Pressable onPress={() => navigation.goBack()} style={styles.btn}>
-      <Text style={styles.text}>back</Text>
+      <Icon name="arrow-back-circle" size={50} color="#4ecdc4" />
     </Pressable>
   );
 };
@@ -16,17 +17,7 @@ export default GoBack;
 
 const styles = StyleSheet.create({
   btn: {
-    backgroundColor: '#4ecdc4',
-    padding: 15,
-    alignItems: 'center',
-    justifyContent: 'center',
-    borderRadius: 10,
     cursor: 'pointer',
     alignSelf: 'flex-start',
-  },
-  text: {
-    color: '#fff',
-    textTransform: 'capitalize',
-    fontWeight: '500',
   },
 });
