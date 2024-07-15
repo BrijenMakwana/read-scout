73289455b28f44a6e6a031ca4285793cdb4331ca
BrijenMakwana <brijenma@gmail.com>
2024-07-15 12:41:36 +0530

    env

--- src/hooks/useAI.tsx
+++ src/hooks/useAI.tsx
@@ -1,7 +1,7 @@
 import {useQuery} from '@tanstack/react-query';
 import {GoogleGenerativeAI} from '@google/generative-ai';
 
-const genAI = new GoogleGenerativeAI('');
+const genAI = new GoogleGenerativeAI(process.env.API_KEY);
 
 const model = genAI.getGenerativeModel({model: 'gemini-1.5-flash'});
 
