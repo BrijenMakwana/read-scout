20a719035ed90a73de706a7af2009adf38615efb
BrijenMakwana <brijenma@gmail.com>
2024-07-16 11:06:02 +0530

    default image url in BookItem

--- src/components/BookItem.tsx
+++ src/components/BookItem.tsx
@@ -38,7 +38,7 @@ const BookItem = (props: IBookItem) => {
         source={{
           uri:
             imageLinks?.thumbnail ||
-            'https://m.media-amazon.com/images/I/41zoxjP9lcL._AC_SY200_QL15_.jpg',
+            'https://images.unsplash.com/photo-1585896452649-6ede5e126800?q=80&w=1974&auto=format&fit=crop&ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D',
         }}
         style={styles.image}
       />
